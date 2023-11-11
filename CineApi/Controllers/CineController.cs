9db893d5d8c2c6785a63a9ae610e3c8702916d07
MinunLast <lautarodaniellop@hotmail.com>
2023-11-11 20:09:22 -0300

    modifique la creacion de la tabla usuarios en la base de datos,cree la entidad usuarios, comence con la logica en el formulario ingresar y en la apiweb para la verificacion del usuario que intenta ingresar

--- CineApi/Controllers/CineController.cs
+++ CineApi/Controllers/CineController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CineBack.Entidades;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CineApi.Controllers
 {
@@ -11,5 +12,33 @@ namespace CineApi.Controllers
         {
             return Ok();
         }
+    }
+
+    [HttpPost("verificar")]    //Verificar que el usuario y la contraseña existan para iniciar sesion 
+    public IActionResult VerificarUsuario([FromBody] Usuarios credenciales)
+    {
+        // Aquí deberías realizar la lógica para verificar las credenciales en la base de datos
+        // Puedes utilizar Entity Framework, Dapper, o cualquier otro ORM o consulta directa a la base de datos
+
+        // Ejemplo básico de verificación
+        if (UsuarioValido(credenciales.Usuario, credenciales.Contraseña))
+        {
+            return Ok("Credenciales válidas");
+        }
+        else
+        {
+            return BadRequest("Credenciales inválidas");
+        }
+    }
+
+    private bool UsuarioValido(string usuario, string contraseña)
+    {
+        // Aquí deberías implementar la lógica para verificar las credenciales en tu base de datos
+        // Puedes utilizar Entity Framework, Dapper, o cualquier otro ORM o consulta directa a la base de datos
+        // Devuelve true si las credenciales son válidas, de lo contrario, devuelve false
+        // Este método es solo un ejemplo, debes adaptarlo a tu aplicación y base de datos específicas
+        // También, asegúrate de manejar las contraseñas de forma segura, por ejemplo, almacenando hashes en lugar de contraseñas en texto plano
+        // y utilizando sal para mayor seguridad
+        return (usuario == "usuarioEjemplo" && contraseña == "contraseñaEjemplo");
     }
 }
